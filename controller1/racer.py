1cca01919ff3b9f26aa984a739e0235e5e64b914
Daniel Kelling <daniel.horus@gmail.com>
2017-05-24 23:04:55 -0300

    Racer init now copies the initializer list, and it has a fitness member

--- controller1/racer.py
+++ controller1/racer.py
@@ -5,17 +5,18 @@ class Racer:
     def __init__(self, n_features=-1, theta=None):
         '''
         Use either n_features != -1 to init an empty racer with all the needed thetas,
-        or data to use the given array of thetas
+        or theta to use the given array of thetas
         '''
 
         self.__theta = []
+        self.__fitness = None
 
         if n_features != -1:
             n_thetas = (n_features + 1) * 5
             self.__theta = [0] * n_thetas
 
         if theta is not None:
-            self.__theta = theta
+            self.__theta = copy.copy(theta)
 
     @property
     def theta(self) -> list:
@@ -35,3 +36,10 @@ class Racer:
         data = copy.copy(r1.theta)
         r2 = Racer(-1, data)   # ignore n_features
         return r2
+
+    @property
+    def fitness(self, controller):
+        if self.__fitness is None:
+            self.__fitness = controller.run_episodes(self.__thetas)
+
+        return self.__fitness
