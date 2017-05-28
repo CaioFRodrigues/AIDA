49657bec18afd2f98901d2c013501fa45eedd04b
Daniel Kelling <daniel.horus@gmail.com>
2017-05-28 03:07:23 -0300

    New features. Didn't include opponent-related sensors yet.

--- controller1/controller.py
+++ controller1/controller.py
@@ -49,87 +49,37 @@ class Controller(controller_template.Controller):
         :return: A list containing the features you defined
         """
 
-        # features[0] => constant 1.0
-        #                   used to multiply the raw affinity for an action
+        d_left, d_front, d_right = sensors[:3]
 
-        # features[1] => change in distance to checkpoint_distance
-        #                   if positive: getting away from checkpoint
-        #                   if negative: getting closer to checkpoint
+        feature = [1.0, 0, 0, 0, 0]
 
-        # features[2] => angle between car movement and left distance to grass
-        #                   if positive: getting away from the grass
-        #                   if negative: getting closer to the grass
-        #                   the smallest the angle, the more it needs to turn right
+        # incentives to accelerate
+        feature[1] = 0.0
+        if d_left < 100 and d_right < 100:
+            feature[1] += 0.5
+        if d_front == 100:
+            feature[1] += 0.5
 
-        # features[3] => change in distance to grass from the right side
-        #                   if positive: getting away from the grass
-        #                   if negative: getting closer to the grass
-        #                   the smallest the angle, the more it needs to turn left
+        # incentives to turn left
+        feature[2] = 0.0
+        if d_right < 100 and d_front < 100:
+            feature[2] += 0.5
+        if d_left == 100:
+            feature[2] += 0.5
 
-        # features[4] => adversary proximity
-        #                   if zero: not close enough to make the enemy the priority instead of the track
-        #                   if positive: getting further away from the enemy_detected
-        #                   if negative: getting closer to the enemy_detected
-        #                   doesn't matter if adversary is in the front or behind, the closer the more
-        #                   it needs to accelerate
+        # incentives to turn right
+        feature[3] = 0.0
+        if d_left < 100 and d_front < 100:
+            feature[3] += 0.5
+        if d_right == 100:
+            feature[3] += 0.5
 
-        # features[5] => attempt to pass adversary in front_vector
-        #               needs to have a little more weight than the grass proximity feature if this is not zero
-        #               if positive: turn to the left
-        #               if negative: turn to the right
+        # incentives to break
+        feature[4] = 0.0
+        if d_front < 100:
+            feature[4] += 1
 
-        # possible feature[6] => attempt to stay as close as possible to position 1/-1 to the adversary
-        # possible feature[7] => keep as close to the right edge as possible
-
-        features = [1.0]
-        if len(self.previous_sensor_values) > 0:
-
-            # features[1]
-                features.append(sensors[1] - self.previous_sensor_values[1])
-
-            # features[2]
-                front_vector = sensors[1] - self.previous_sensor_values[1]
-                left_vector = sensors[0] - self.previous_sensor_values[0]
-                if sqrt(front_vector ** 2 + left_vector ** 2) != 0:
-                    angle = arcsin(front_vector / sqrt(front_vector ** 2 + left_vector ** 2))
-                    features.append(angle)
-                else:
-                    features.append(1)
-
-            # features[3]
-                front_vector = sensors[1] - self.previous_sensor_values[1]
-                right_vector = sensors[2] - self.previous_sensor_values[2]
-                if sqrt(front_vector ** 2 + right_vector ** 2) != 0:
-                    angle = arcsin(front_vector / sqrt(front_vector ** 2 + right_vector ** 2))
-                    features.append(angle)
-                else:
-                    features.append(1)
-
-            # features[4]
-                if sensors[8] == 0:
-                    features.append(0)
-                else:
-                    features.append(sensors[6] - self.previous_sensor_values[6])
-
-            # features[5]
-                if sensors[8] == 0:
-                    features.append(0)
-                else:
-                    # Negative: coming from the left / positive: coming from the right
-                    # Absolute value larger than 1: coming from behind / else: coming from the front
-                    enemy_position = sensors[7] / 90.0
-                    if (abs(enemy_position) > 1):
-                        features.append(0)
-                    else:
-                        features.append(enemy_position)
-
-
-        else:
-            features = [1.0, sensors[4], sensors[0], sensors[2], 0, 0] # No previous values, the features just return the current sensor values
-
-        self.previous_sensor_values = [value for value in sensors]
-        return features
-
+        return feature
 
     def learn(self, weights) -> list:
         """
@@ -142,5 +92,5 @@ class Controller(controller_template.Controller):
         self.no_bot_state = Simulation(self.track_name, None)
         self.parked_bots_state = Simulation(self.track_name, 'parked_bots')
         self.ninja_bot_state = Simulation(self.track_name, 'ninja_bot')
-        evo = Evolution(max_population_size=60, n_actions=5, n_features=6, adam_genes=weights, best_overall=True)
+        evo = Evolution(max_population_size=60, n_actions=5, n_features=5, adam_genes=weights, best_overall=False)
         winner = evo.evolve(generations=500, controller=self)
