ff5f99cc9512b12853e30872a94b8c66924c5336
Caio_Rodrigues <caio_f.r@hotmail.com>
2017-05-28 21:59:59 -0300

    Hill Climbing and simulated annealing refatoration

--- controller1/simulated_annealing.py
+++ controller1/simulated_annealing.py
@@ -11,15 +11,15 @@ class Simulated_Annealing:
         weights = anneal.simulate(self)
     '''
     def __init__ (self, weights,controller):
-        self.__weights = deepcopy(weights)
+        self.__weights = weights
         self.__result = controller.run_episode(weights) 
         self.__temp = 100
         
     def simulate (self, controller):
         while (self.__temp != 0):
             self.__weights = self.__get_neightbor(controller)
-            self.__temp = self.__temp - 10
-        
+            self.__temp = self.__temp - 1
+            print (self.__result)
         return self.__weights
  
     def __get_neightbor(self, controller)->list:
@@ -27,66 +27,46 @@ class Simulated_Annealing:
         Generate the nearest thetas for the function, for each weight passed it will try to increase and decrease,
         returning a list with twice the size
         '''
-        weights = deepcopy(self.__weights)
-        N = 100
+        weights = self.__weights
+        N = 10
         for i in range(0,N):
-            weights = deepcopy(self.__weights)
-            
-            
-            ''' 
-            index defines a random number that goes from 0 to the double of the size of the list of weights.
-            This is so if random is over the list of weights, it is then made the operation of - instead of +
-            '''
-            index = randint(0,(len(self.__weights) * 2 - 1)) 
-
-
-            
-            if (index >= len(self.__weights)):
-                position = index - len(self.__weights)
-                weights[position] = weights[position] - 1
-                result = controller.run_episode(weights)
-                if (self.__choose_to_change(result,controller) == True):
-                    self.__weights = weights
-                    self.__result = result
-            else:
-                position = index
-                weights[position] = weights[position] + 1
-                result = controller.run_episode(weights)
-                if (self.__choose_to_change(result,controller)):
-                    self.__weights = weights
-                    self.__result = result
-            
-            
-
+            weights = self.__weights
+          
      
+            index = randint(0,(len(self.__weights)-1)) #Defines the index to be accessed 
+            op = randint(0,1) #Decides which operation will be made
+            if op == 1:
+                weights[index] = weights[index] - 0.1
+            else:
+                weights[index] = weights[index] + 0.1
+                
+            result = controller.run_episode(weights)
+            if (self.__choose_to_change(result,controller) == True):
+                #Updates the current weights and the result
+                self.__weights = weights
+                self.__result = result
+           
         return self.__weights
     
        
     def __choose_to_change(self,result, controller):
         '''
-        Decides whether or not the current weights will changed based on the formula
+        Decides whether or not the current weights will changed based on the temperature
         '''
+
         delta = result - self.__result 
-        
-        print ("Delta:")
-        print (delta)
-        print ("Result:")
-        print (self.__temp)
-        print (result)
-        print(self.__result)
         if delta == 0:
             return False
         if (delta > 0):
             return True
         else:
             percent = exp(delta/self.__temp)
-            
+            print (percent)
             rand =  randrange(0,100)
 
             if (percent < rand):
                 return False
             else:
-                print ("Went somewhere worse")
                 return True
                 
         
